04da315984792f49353af7f0a04ddc07f6b19e93
MohamedAtef308 <mimo3082002@gmail.com>
2023-06-27 19:39:47 +0300

    Layout modified

--- Code.py
+++ Code.py
@@ -1,9 +1,10 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMainWindow,
                              QComboBox, QLabel, QHBoxLayout, QLineEdit)
-from PyQt5.QtGui import QIcon, QColor, QPalette
+from PyQt5.QtGui import QIcon, QColor, QPalette, QPixmap
 from pytube import YouTube
 from pythumb import Thumbnail
 import os
+import socket
 
 
 def get_thumbnail(thumbnail_url):
@@ -38,15 +39,21 @@ def get_thumbnail(thumbnail_url):
 
 
 def convert_video(conversion_url):
+    # check for network connection
+    try:
+        socket.create_connection(("8.8.8.8", 53))
+    except OSError:
+        return False, False
+
     # get the YouTube object
     try:
         # make sure no error while converting
-        conversion_streams = YouTube(conversion_url).streams
-        return conversion_streams
+        conversion_streams = YouTube(conversion_url)
+        return conversion_streams.streams, conversion_streams.title
 
     # return none in case of error
     except Exception:
-        return None
+        return None, None
 
 
 def get_streams(streams_yt):
@@ -106,7 +113,7 @@ def build_app():
     building_window.setWindowIcon(icon)
 
     # set window place, size and background color
-    building_window.setGeometry(400, 300, 700, 450)
+    building_window.setFixedSize(700,450)
     palette = QPalette()
     palette.setColor(QPalette.Window, QColor(25, 25, 25))
     building_window.setPalette(palette)
@@ -120,19 +127,28 @@ def build_app():
 
     # create the first row of elements (text box, convert button)
     link_input = QLineEdit()
+    link_input.setFixedHeight(45)
     convert_button = QPushButton("Convert")
+    convert_button.setFixedHeight(45)
 
     # put them in a horizontal layout
     text_button_layout.addWidget(link_input)
     text_button_layout.addWidget(convert_button)
 
-    # add the horizontal layout to the vertical layout
+    # add a title label and the horizontal layout to the vertical layout
+    title_label = QLabel("")
+    title_label.setStyleSheet("background-color: red;")
+    title_label.setFixedHeight(45)
+    input_layout.addWidget(title_label)
     input_layout.addLayout(text_button_layout)
 
     # create the second row of elements (combo box, status label, download button)
     download_button = QPushButton("Download")
+    download_button.setFixedHeight(45)
     status_label = QLabel("")
+    status_label.setFixedHeight(45)
     quality_box = QComboBox()
+    quality_box.setFixedHeight(45)
 
     # make the download button inactive by default
     download_button.setEnabled(False)
@@ -149,11 +165,12 @@ def build_app():
     # put the widget at the center of the window
     building_window.setCentralWidget(main_widget)
 
-    return building_app, building_window, link_input, convert_button, download_button, quality_box, status_label
+    return (building_app, building_window, link_input, convert_button,
+            download_button, quality_box, status_label, title_label)
 
 
-def conversion_start(text_field, status_msg, download_button, quality_list):
-    # upadte status label
+def conversion_start(text_field, status_msg, download_button, quality_list, title_label):
+    # update status label
     status_msg.setStyleSheet("color: #ffce45;")
     status_msg.setText("Converting...")
     status_msg.repaint()
@@ -163,12 +180,24 @@ def conversion_start(text_field, status_msg, download_button, quality_list):
 
     # to get access to the global variable instead of creating a new one and pass the streams to it
     global streams
-    streams = convert_video(conversion_text)
+    streams, vid_title = convert_video(conversion_text)
 
     # if the function couldn't process the URL
     if streams is None:
         status_msg.setStyleSheet("color: #6e0d25;")
         status_msg.setText("Conversion Error!")
+        status_msg.repaint()
+
+        # disable downloading when an error happens
+        download_button.setEnabled(False)
+        quality_list.setEnabled(False)
+        quality_list.clear()
+        return
+
+    # if there's no internet connection
+    elif streams is False:
+        status_msg.setStyleSheet("color: #6e0d25;")
+        status_msg.setText("Connection Error!")
         status_msg.repaint()
 
         # disable downloading when an error happens
@@ -207,10 +236,13 @@ def conversion_start(text_field, status_msg, download_button, quality_list):
     quality_list.update()
     download_button.update()
 
-    # update the status label
+    # update the status label and title label
     status_msg.setStyleSheet("color: #306844;")
     status_msg.setText("Conversion Done")
     status_msg.repaint()
+    title_label.setStyleSheet("color: #306844;")
+    title_label.setText(vid_title)
+    title_label.repaint()
 
 
 def download_start(download_streams, status_msg, quality_list, converted_streams_list):
@@ -234,13 +266,17 @@ def download_start(download_streams, status_msg, quality_list, converted_streams
 
 
 # -------------------------- Building App --------------------------
-app, window, url_input, convert_but, download_but, qlty_list, status = build_app()
+app, window, url_input, convert_but, download_but, qlty_list, status, title = build_app()
 streams = None
 streams_list = None
 
 window.show()
 
-convert_but.clicked.connect(lambda: conversion_start(url_input, status, download_but, qlty_list))
+# pic = QPixmap("thumbnail.jpg")
+# title.setPixmap(pic)
+# title.setScaledContents(True)
+
+convert_but.clicked.connect(lambda: conversion_start(url_input, status, download_but, qlty_list, title))
 download_but.clicked.connect(lambda: download_start(streams, status, qlty_list, streams_list))
 
 app.exec_()
